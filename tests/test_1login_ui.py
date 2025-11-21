95dac43869276420642fd616ac86fc7590664e9e
Silumi <silumidayawardana@gmail.com>
2025-11-21 11:50:29 +0000

    test

--- tests/test_1login_ui.py
+++ tests/test_1login_ui.py
@@ -6,8 +6,8 @@ def test_valid_login(page):
     """Test valid login with standard_user"""
     page.goto("https://www.saucedemo.com/", timeout=30000)
     
-    page.fill("#user-name", "standard_user")
+    page.fill("#user-name", "standard_user1")
     page.fill("#password", "secret_sauce")
     page.click("#login-button")
-    assert "inventory" in page.url, "Login failed.nothing"
+    assert "inventory" in page.url, "Login failed"
     # test
